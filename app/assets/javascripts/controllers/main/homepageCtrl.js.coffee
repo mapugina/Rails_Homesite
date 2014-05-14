f567bda358f8b53a96a9b4f8294c1f2875f41663
mapugina <mapugina@gmail.com>
2014-05-14 17:10:49 -0600

    Inital commit. Rails routes setup but waiting on angular routes

---
+++ app/assets/javascripts/controllers/main/homepageCtrl.js.coffee
@@ -0,0 +1,2 @@
+@HomepageCtrl = ($scope) ->
+	
---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+RailsHomesite::Application.config.secret_key_base = '9021ff54729a9e7efbdfb5132dae2c706ce62dd4309f8eb83989f5d5bb19f93bcda037c581ecbca5ad1b3bf81e2cb6b2edcd68934e24c2e7883a7959fc7bc5e1'
