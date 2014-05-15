f5d2624959e2591b105838a1545fe99fae5ef41e
mapugina <mapugina@gmail.com>
2014-05-15 10:50:11 -0600

    Added json response

--- app/controllers/projects_controller.rb
+++ app/controllers/projects_controller.rb
@@ -1,2 +1,16 @@
 class ProjectsController < ApplicationController
+  respond_to :json
+  
+  def index
+    
+    #gather all projects
+    projects = Project.all
+    
+    #respond to request with post data in json format
+    respond_with(projects) do |format|
+      format.json { render :json => projects.as_json }
+    end
+    
+  end
+  
 end
